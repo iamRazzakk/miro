70e56335022302b35d0715b0dbf8bf459a494d2f
Md Abdur Razzak ~ <rakibt23p@gmail.com>
2024-03-02 21:55:12 +0600

    work on header section

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,14 @@
 
 import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
+import Footer from './Footer';
 
 const Layout = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import BuiltForAllKinds from "./BuiltForAllKinds";
 import Collaborate from "./Collaborate";
 import Connect from "./Connect";
+import Header from "./Header";
 import TrustedUser from "./TrustedUser";
 import WayYouWork from "./WayYouWork";
 import WeWork from "./WeWork";
@@ -11,7 +12,9 @@ import WorldBestTeam from "./WorldBestTeam";
 const Home = () => {
     return (
         <div>
-            <h1 className="text-5xl font-bold">This is from home</h1>
+            {/* <h1 className="text-5xl font-bold">This is from home</h1>
+             */}
+            <Header></Header>
             <TrustedUser></TrustedUser>
             <Collaborate></Collaborate>
             <div className="md:w-[1020px]">
