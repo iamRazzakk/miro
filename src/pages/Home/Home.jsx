09bb7e7afdf863350f582627e4b90d9239bc3bba
Md Abdur Razzak ~ <rakibt23p@gmail.com>
2024-03-02 09:19:07 +0600

    Add trustedUser Component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
+import TrustedUser from "./TrustedUser";
 
 
 const Home = () => {
     return (
         <div>
             <h1 className="text-5xl font-bold">This is from home</h1>
-
+            <TrustedUser></TrustedUser>
         </div>
 
     );
